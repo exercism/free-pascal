432fecbb244ebe0f8555c59f4a830dfb92d80903
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-10-03 23:15:56 -0300

    * add practice exercise: scrabble-score

---
+++ exercises/practice/scrabble-score/.meta/example.pas
@@ -0,0 +1,40 @@
+Unit ScrabbleScore;
+
+{$mode ObjFPC}{$H+}
+
+Interface
+
+Function RunExercise(word: String) :   Integer;
+
+Implementation
+
+Uses SysUtils;
+
+Var
+    i, score    :   Integer;
+    temp :   String;
+
+Function RunExercise(word: String) :   Integer;
+Begin
+
+    score := 0;
+    temp := UpperCase(word);
+
+    For i := 1 To Length(temp) Do
+        Begin
+            Case Copy(temp, i, 1) Of
+                'A','E','I','O','U','L','N','R','S','T' :   score := score +  1;
+                'D','G'                                 :   score := score +  2;
+                'B','C','M','P'                         :   score := score +  3;
+                'F','H','V','W','Y'                     :   score := score +  4;
+                'K'                                     :   score := score +  5;
+                'J','X'                                 :   score := score +  8;
+                'Q','Z'                                 :   score := score + 10;
+            End;
+        End;
+
+    Result := score;
+
+End;
+
+End.
---
+++ exercises/practice/scrabble-score/ScrabbleScore.pas
@@ -0,0 +1,19 @@
+Unit ScrabbleScore;
+
+{$mode ObjFPC}{$H+}
+
+Interface
+
+Function RunExercise(word: String) :   Integer;
+
+Implementation
+
+Function RunExercise(word: String) :   Integer;
+Begin
+
+   // YOUR CODE GOES HERE
+
+End;
+
+End.
+
