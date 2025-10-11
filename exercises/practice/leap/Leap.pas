05a591d4d530fb7f5ff780c448fb75b623bca9f2
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-10-11 18:38:29 -0300

    * improving comments

--- exercises/practice/leap/Leap.pas
+++ exercises/practice/leap/Leap.pas
@@ -11,6 +11,7 @@ Function RunExercise(Const year :  Integer) :   Boolean;
 Begin
 
    { WRITE YOUR SOLUTION }
+   // Try writing a simple algorithm using conditional expressions instead of the built-in function.
 
 End;
 
